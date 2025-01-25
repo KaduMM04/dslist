c32292c71a6017b2d9909e8803e383efa583e961
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-01-25 15:01:44 -0300

    Game seed

--- src/main/java/com/mirandakadu/dslist/entities/Game.java
+++ src/main/java/com/mirandakadu/dslist/entities/Game.java
@@ -2,7 +2,6 @@ package com.mirandakadu.dslist.entities;
 
 import java.util.Objects;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,7 +22,7 @@ public class Game {
 	@Column(name="game_year")
 	private Integer year;
 	
-	private String gener;
+	private String genre;
 	
 	private String platforms;
 	
@@ -31,19 +30,21 @@ public class Game {
 	
 	private String imgUrl;
 	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
 	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
 	}
 	
-	public Game(Long id, String title, Integer year, String gener, String platforms, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
-		this.gener = gener;
+		this.genre = genre;
 		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
@@ -76,11 +77,11 @@ public class Game {
 	}
 
 	public String getGener() {
-		return gener;
+		return genre;
 	}
 
-	public void setGener(String gener) {
-		this.gener = gener;
+	public void setGener(String genre) {
+		this.genre = genre;
 	}
 
 	public String getPlatforms() {
