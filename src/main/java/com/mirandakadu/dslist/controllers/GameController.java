36ce798907ac9926982489ed480d1bebaacb7711
Kadu Miranda <kadumendesmiranda@gmail.com>
2025-01-25 17:16:14 -0300

    GET game by id

--- src/main/java/com/mirandakadu/dslist/controllers/GameController.java
+++ src/main/java/com/mirandakadu/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mirandakadu.dslist.dtos.GameDTO;
 import com.mirandakadu.dslist.dtos.GameMinDTO;
 import com.mirandakadu.dslist.services.GameService;
 
@@ -16,6 +18,12 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value ="/{id}")
+	public GameDTO findById(@PathVariable Long id){
+		GameDTO result = gameService.findGameById(id);
+		return result;
+	}
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
